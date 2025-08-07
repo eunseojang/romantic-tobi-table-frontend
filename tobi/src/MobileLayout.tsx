763e5c18b75501c2ab48c459b61ec71efb6ffb5b
eunseo <jangeun4097@naver.com>
2025-08-07 21:22:14 +0900

    Update MobileLayout.tsx

--- tobi/src/MobileLayout.tsx
+++ tobi/src/MobileLayout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export const MobileLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <Box maxW="390px" w="100%" mx="auto" minH="100vh" bg="red" boxShadow="md">
+    <Box maxW="390px" w="100%" mx="auto" minH="100vh" bg="red">
       {children}
     </Box>
   );
